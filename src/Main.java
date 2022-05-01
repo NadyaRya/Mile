f5b63811f4709ac9f0fd2a252072df722f913431
NadyaRya <nadya_rya@mail.ru>
2022-05-01 17:35:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int airFare = 7000;
+        int mile = 20;
+        int mileQuantity = airFare / mile;
+        System.out.println("Количество бонусных миль:" + mileQuantity);
+    }
+}
